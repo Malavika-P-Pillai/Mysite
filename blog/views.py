f336379bc6dc9300c9f18f3040d2cf9e7ffccfa5
Malavika-P-Pillai <sreelakamani98@gmail.com>
2017-06-07 15:17:19 +0530

    MYSITE

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponseRedirect
+
+
+
+
 import generic as generic
 from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
 
 from django.shortcuts import render, redirect
-from django.views import generic
+from django.template import context
+from django.views import generic, View
 # Create your views here.
 from blog.models import Post, Comment
 
@@ -23,13 +29,72 @@ class PostDetailView(generic.DetailView):
 
         comment = Comment.objects.filter(post=context['post'])
         context['comments'] = comment
+        return context
+    def add_comment(self,request):
+        if request.method == "POST":
+            post = context['post']
+            user = User.objects.get(username=request.user.username)
+            comment_text = request.POST['comment_text']
+            new_comment = Comment(comment_text=comment_text, user=user)
+            new_comment.save()
+            return redirect('view_post', post.id)
 
+def signup(request):
+    template = 'registration/signup.html'
+    context = {}
+    if request.method == 'POST':
+        firstname = request.POST ['firstname']
+        lastname = request.POST ['lastname']
+        email = request.POST ['email']
+        username = request.POST ['username']
+        password1 = request.POST ['password1']
+        password2 = request.POST ['password2']
+
+        user = User.objects.filter(email=email)
+        if len(user) != 0:
+            context['errors'] = "Email is already taken"
+            return render(request, template, context)
+
+        user = User.objects.filter(username=username)
+        if len(user) != 0:
+            context['errors'] = "Username is already taken"
+            return render(request, template, context)
+
+        if password1 == password2:
+            user = User(first_name=firstname, last_name=lastname, email=email, username=username)
+            user.set_password(password1)
+            user.save()
+            return redirect('login')
+
+    return render(request, template, context)
+
+
+
+    """def add_comment(self,request):
         if request.method == "POST":
             comment_text = request.POST['comment']
             user = User.objects.get(username=request.user.username)
-            post = Comment(comment_text=comment_text, user=user)
-            return redirect('view_post', post.id)
-        return context
+            comment = Comment.objects.filter(post=context['post'])
+            comment= Comment(comment_text=comment_text, user=user)
+            return redirect('view_post', post..id)"""
+
+"""class MyFormView(View):
+    form_class = Comment
+    initial = {'comment_text': 'value'}
+    template_name = 'blog/view.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'comment': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            return HttpResponseRedirect('/blog')
+
+        return render(request, self.template_name, {'form': form}"""
 
 
 def add_post(request):
@@ -76,6 +141,15 @@ def del_post(request,pk):
         raise PermissionDenied
     if post is not None:
         post.delete()
+        return redirect('Home')
+
+def del_comment(request,pk):
+    post = Post.objects.get(id=int(pk))
+    comment = Comment.objects.filter(post=post)
+    if request.user.username != post.user.username:
+        raise PermissionDenied
+    if comment is not None:
+        comment.delete()
         return redirect('Home')
 
 
